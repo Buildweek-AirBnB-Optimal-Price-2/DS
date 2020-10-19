fc3cefdbf215bdff8a8d1b28b0de1adda1a19da5
Luke Melto <lukiepookieofficial@users.noreply.github.com>
2020-10-18 23:00:34 -0500

    instantiate model

--- airbnb_clone/model.py
+++ airbnb_clone/model.py
@@ -8,14 +8,38 @@ import numpy as np
 '''Predicts airbnb rental price based on parameters.'''
 
 
-def predict_price(rental_parameters):
+def predict_price(model, rental_parameters):
     """Predicts airbnb rental price based on parameters.
 
     Args:
+        model (LinReg): LinearRegression model from train_model()
         rental_parameters (list): List of user-selected parameters
 
     Returns:
         float: Predicted price of rental property
     """
 
-    return rental_parameters
+    #                    [[lon,                  lat,                  min_nights]]
+    return model.predict([[rental_parameters[0], rental_parameters[1], rental_parameters[2]]])
+
+
+def train_model():
+    """Instanciates and trains the model.
+    """
+
+    url = 'https://raw.githubusercontent.com/lukiepookieofficial/NYC-AirBNB/main/AB_NYC_2019.csv'
+    df = pd.read_csv(url)
+
+    features = ['longitude', 'latitude', 'minimum_nights']
+    target = ['price']
+
+    x_train = df[features]
+    y_train = df[target]
+
+    X_train, _X_test, y_train, _y_test = train_test_split(x_train, y_train, test_size=0.25,
+                                                          random_state=1138)
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    return model
